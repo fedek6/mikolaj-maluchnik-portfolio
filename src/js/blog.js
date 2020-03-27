f10cf9f4d6f0d8d338fe93f86eb62274a8fd64f3
Konrad Fedorczyk <fedek6@gmail.com>
2020-03-27 08:55:25 +0100

    Efekty dla blogowych teaserów

--- src/js/blog.js
+++ src/js/blog.js
@@ -11,3 +11,20 @@
 
 // Little hello world to test compilers/transpilers 
 console.log('Running blog module.')
+
+document.addEventListener('mouseover', (event) => {
+
+	if (event.target.classList.contains('js-content-loader')) {
+        let contentElement = event.target.parentNode.nextElementSibling
+        contentElement.classList.add('hover')
+	}
+}, false);
+
+
+document.addEventListener('mouseout', (event) => {
+
+	if (event.target.classList.contains('js-content-loader')) {
+        let contentElement = event.target.parentNode.nextElementSibling
+        contentElement.classList.remove('hover')
+	}
+}, false);
